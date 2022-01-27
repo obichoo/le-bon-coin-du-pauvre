4e8213eaab3d3ab9d466588be9fa77214a04dc59
M0byfloyd <alexandre.robert2810@gmail.com>
2022-01-27 16:33:51 +0100

    Le système de vote du pauvre

---
+++ migrations/prout-Version20220127141508.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220127141508 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE ad (id INT AUTO_INCREMENT NOT NULL, tags_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(1000) DEFAULT NULL, price DOUBLE PRECISION DEFAULT NULL, votes INT NOT NULL, slug VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_77E0ED58989D9B62 (slug), INDEX IDX_77E0ED588D7B4FB4 (tags_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ad ADD CONSTRAINT FK_77E0ED588D7B4FB4 FOREIGN KEY (tags_id) REFERENCES ad (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ad DROP FOREIGN KEY FK_77E0ED588D7B4FB4');
+        $this->addSql('DROP TABLE ad');
+        $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE user');
+    }
+}
